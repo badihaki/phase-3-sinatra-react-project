c80ef34e19de629ff4f492e0649f27a23e735e8d
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-10 12:48:16 -0400

    restaurant and bartender getter routes are ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,27 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  
+  # Lets do Restaurant routes here
+  get "/restaurants" do
+    all_restaurants = Restaurant.all
+    all_restaurants.to_json
+  end
+
+  get "/restaurants/:id" do
+    restaurant = Restaurant.find(params[:id])
+    restaurant.to_json
+  end
+
+  # Lets rock out bartender route here
+  get "/bartenders" do
+    all_bartenders = Bartender.all
+    all_bartenders.to_json
+  end
+
+  get "/bartenders/:id" do
+    bartender = Bartender.find(params[:id])
+    bartender.to_json
+  end
 
 end
